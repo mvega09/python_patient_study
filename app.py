3d2a835298531ff59a3a39f14fdcda144d4dfd60
Mateo Vega <matiusve09@outlook.com>
2024-09-08 17:51:56 -0500

    Se agregan los archivos modificados el codigo queda completamente funcional

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
-import os
 import streamlit as st
 import pandas as pd
-import mysql.connector
-from mysql.connector import Error
+from patients_to_db import insert_patients_to_db
+from studies_to_db import insert_studies_to_db
+from  combined_to_db import insert_combined_to_db
 
 load_dotenv()
 
@@ -46,54 +46,17 @@ def process_and_combine_excel_files(excel_files):
             st.write(f"Error reading the Excel file {file.name}: {e}")
             continue
 
-    # Verificar que los DataFrames tienen datos
     if patient_df.empty:
         st.error("Error: No se encontraron datos de pacientes. Por favor, sube el archivo correcto.")
     if study_df.empty:
         st.error("Error: No se encontraron datos de estudios. Por favor, sube el archivo correcto.")
     
     if patient_df.empty or study_df.empty:
-        return None, patient_df, study_df  # Retornar None si falta algún DataFrame
+        return None, patient_df, study_df
 
-    # Combinar ambos DataFrames basados en los campos 'document'
     combined_df = pd.merge(patient_df, study_df, on='document', how='inner')
 
     return combined_df, patient_df, study_df
-
-def insert_data_to_db(table_name, dataframe, columns):
-    """Inserts data into the specified table in the MySQL database."""
-    try:
-        connection = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME")
-        )
-
-        cursor = connection.cursor()
-
-        # Preparar la consulta SQL
-        placeholders = ', '.join(['%s'] * len(columns))
-        insert_query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({placeholders})"
-
-        # Insertar los datos
-        data_to_insert = dataframe[columns].to_records(index=False).tolist()
-        cursor.executemany(insert_query, data_to_insert)
-
-        connection.commit()
-
-        st.success(f"{cursor.rowcount} rows inserted successfully into {table_name}.")
-
-    except Error as e:
-        st.error(f"Error: {e}")
-        if connection:
-            connection.rollback()
-
-    finally:
-        if cursor:
-            cursor.close()
-        if connection and connection.is_connected():
-            connection.close()
 
 st.title("Upload patient and study data")
 
@@ -104,22 +67,20 @@ if uploaded_files:
     combined_df, patient_df, study_df = process_and_combine_excel_files(uploaded_files)
     
     if patient_df is not None and study_df is not None:
-        # Mostrar los DataFrames de pacientes y estudios por separado
         st.write("Datos de pacientes cargados:")
         st.dataframe(patient_df)
         if st.button('Guardar datos de pacientes en la base de datos'):
-            insert_data_to_db('patients', patient_df, ['full_name', 'document_type', 'document', 'birthdate', 'patient_sex', 'phone_number', 'email'])
+            insert_patients_to_db(patient_df)
         
         st.write("Datos de estudios cargados:")
         st.dataframe(study_df)
         if st.button('Guardar datos de estudios en la base de datos'):
-            insert_data_to_db('studies', study_df, ['modality', 'study', 'study_carried_out', 'price'])
+            insert_studies_to_db(study_df)
         
-        # Mostrar el DataFrame combinado si existe
         if combined_df is not None:
             st.write("Datos combinados de pacientes y estudios:")
             st.dataframe(combined_df)
             if st.button('Guardar datos combinados en la base de datos'):
-                insert_data_to_db('patient_study_combined', combined_df, ['full_name', 'document_type', 'document', 'birthdate', 'patient_sex', 'phone_number', 'email', 'modality', 'study', 'study_carried_out', 'price'])
+                insert_combined_to_db(combined_df)
         else:
             st.write("No se pudo combinar los datos debido a la falta de datos en uno de los archivos.")
---
+++ patients_to_db.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import os
+import mysql.connector
+from mysql.connector import Error
+
+def insert_patients_to_db(dataframe):
+    """Inserts patient data into the 'patients' table in the MySQL database."""
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        
+        cursor = connection.cursor()
+        
+        insert_query = """
+        INSERT INTO patients (full_name, document_type, document, birthdate, patient_sex, phone_number, email)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        
+        data_to_insert = dataframe[['full_name', 'document_type', 'document', 'birthdate', 'patient_sex', 'phone_number', 'email']].to_records(index=False).tolist()
+        cursor.executemany(insert_query, data_to_insert)
+        
+        connection.commit()
+        st.success(f"{cursor.rowcount} rows inserted successfully into patients.")
+    
+    except Error as e:
+        st.error(f"Error: {e}")
+        if connection:
+            connection.rollback()
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
